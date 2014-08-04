a8b29334f921d1ab761c9b4a937ae11c1ee35cbc
unknown <sagar.chandarana123@gmail.com>
2014-08-04 17:17:12 -0300

    Appbase interface- vertex creation and property-set/delete done.

--- test/unit/userinterfacemethods.js
+++ test/unit/userinterfacemethods.js
@@ -1,16 +1,131 @@
-it.skip('should not allow having null as a vertex property value', function(done) {
-  var path = "Materials/Wood/~properties"
-  var data = {"val":null}
-  async.waterfall([
-    function(callback) {
-      ab.server.vertex.set(domain+path, data, callback)
-    }
-  ], function(err, result) {
-    if(err) {
-      expect(err.message).to.equal("invalid datatype for key val")
-      done()
-    } else {
-      done(new Error('unable to catch invalid datatype'))
-    }
-  })
+var expect = chai.expect
+var baseUrl = "http://app.sagar.appbase.io/"
+
+describe('interface methods', function(){
+
+    describe('appbase.new',function(){
+
+        it("new vertex- with key- should not give an error, and ref should point to the proper path", function(done){
+            var path = baseUrl + "Materials/Wood";
+            Appbase.new(path,function(error,ref){
+                if(!error){
+                    expect(ref.path()).to.be.equal(path);
+                    done();
+                } else {
+                    done(error);
+                }
+
+            })
+        })
+
+        it("new vertex- without key- should not give an error, and ref should point to the proper path", function(done){
+            var path = baseUrl + "Materials";
+            Appbase.new(path,function(error,ref){
+                if(!error){
+                    var refPath = ref.path();
+                    expect(refPath.slice(0,refPath.lastIndexOf('/'))).to.be.equal(path);
+                    done();
+                } else {
+                    done(error);
+                }
+
+            })
+        })
+
+    })
+
+
+    describe('paths',function(){
+
+        var path = baseUrl + "Materials/Wood";
+        var ref = new ab.interface.ref(path);
+
+        it("should return  ref's path path", function(){
+            expect(ref.path()).to.equal(path);
+        })
+
+        it("should return outVertex's ref",function(){
+            var edgeName = 'outV';
+            expect(ref.outVertex(edgeName).path()).to.equal(path+'/'+edgeName);
+        })
+
+        it("should return inVertex's ref",function(){
+            var edgeName = 'outV';
+            var outVRef = ref.outVertex(edgeName);
+            expect(outVRef.inVertex().path()).to.equal(path);
+        })
+
+
+    });
+
+    describe('vertex data',function(){
+
+
+        var path = baseUrl + "Materials/Wood";
+        var ref = new ab.interface.ref(path);
+
+
+
+        it("setData should not throw an error, return the proper reference",function(done){
+            var data = {"color":"brown"};
+
+            async.waterfall([
+                function(callback) {
+                    ref.setData(data, callback);
+                }
+            ], function(err, ref) {
+                if(err)
+                    done(err)
+
+                else {
+                    expect(ref.path()).to.equal(path);
+                    done()
+                }
+            })
+
+        })
+
+        it("removeData- some properties- should not throw an error, return the proper reference",function(done){
+            var data = ["color"];
+
+            async.waterfall([
+                function(callback) {
+                    ref.removeData(data, callback);
+                }
+            ], function(err, ref) {
+                if(err)
+                    done(err)
+
+                else {
+                    expect(ref.path()).to.equal(path);
+                    done()
+                }
+            })
+
+        })
+
+
+        it("removeData- all- should not throw an error, return the proper reference",function(done){
+
+            async.waterfall([
+                function(callback) {
+                    ref.removeData(true, callback);
+                }
+            ], function(err, ref) {
+                if(err)
+                    done(err)
+
+                else {
+                    expect(ref.path()).to.equal(path);
+                    done()
+                }
+            })
+
+        })
+
+    })
+
+
+
+
 })
